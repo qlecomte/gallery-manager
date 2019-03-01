66b2e870624badf6a74ddecfa98db95df0efc073
Quentin Lecomte <quentinh.lecomte@gmail.com>
2019-03-02 00:35:04 +0100

    Add details column

--- app/albums/albums.db.js
+++ app/albums/albums.db.js
@@ -10,7 +10,7 @@ module.exports.getFavoriteAlbums = async () => {
   return knex.select().from('albums').where({ favorite: 1 }).orderBy('createdAt', 'desc')
 }
 module.exports.getPictures = async (id) => {
-  return knex.select().from('pictures').join('albums_pictures', 'pictures.id', 'albums_pictures.pictureId').where({ 'albums_pictures.albumId': id }).orderBy('pictures.takenAt', 'desc')
+  return knex.select().from('pictures').join('albums_pictures', 'pictures.id', 'albums_pictures.pictureId').where({ 'albums_pictures.albumId': id }).orderBy('pictures.takenAt', 'asc')
 }
 module.exports.createAlbum = async (album) => {
   await knex.insert(album).into('albums')
--- public/src/components/PictureDetails.vue
+++ public/src/components/PictureDetails.vue
@@ -2,10 +2,22 @@
     <div>
         <h1 class="container">Picture Details</h1>
         <div>
-            <div class="row">
-                <div class="col-md-2" style="background-color: grey"></div>
+            <div class="row no-gutter">
                 <div class="col-md-10 img-container">
-                    <img class="picture" :src="getPicture(picture.url)"/>
+                    <img class="picture" v-if="picture && picture.url" :src="getPicture(picture.url)"/>
+                </div>
+                <div class="col-md-2 infos" v-if="picture">
+                    <div class="name">{{picture.name}}</div>
+                    <div class="description">{{picture.description}}</div>
+                    <div class="taken">Prise le : {{picture.takenAt | dateFormat }}</div>
+                    <div class="coordinates d-none d-sm-block">{{picture.coordinates}}</div>
+                    <div class="details d-none d-sm-block" v-if="picture.exif">
+                        <div>Modèle : {{picture.exif.Model}}</div>
+                        <div>Ouverture : F/{{picture.exif.SubExif.FNumber[0]}}</div>
+                        <div>Longueur Focale : {{picture.exif.SubExif.FocalLength[0]}} mm</div>
+                        <div>Temps d'exposition : 1/{{1/picture.exif.SubExif.ExposureTime}} s</div>
+                        <div>ISO : {{picture.exif.SubExif.PhotographicSensitivity}}</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -14,12 +26,14 @@
 </template>
 <script>
   import axios from 'axios'
+  import moment from 'moment'
+  moment.locale('fr');
 
   export default {
     name: 'PictureDetails',
     data: function () {
       return {
-        picture: {}
+        picture: null
       }
     },
     computed: {
@@ -40,7 +54,12 @@
           .catch(error => (console.error(error)))
       }
     },
-    beforeMount(){
+    filters: {
+      dateFormat: function (value) {
+          return moment(value).format('DD MMMM YYYY');
+      }
+    },
+    created(){
       this.getPictureDetails(this.$route.params.id)
     }
   }
@@ -57,5 +76,22 @@
     .picture {
         max-height: 100%;
         display: block;
+    }
+    .row.no-gutter {
+        margin-left: 0;
+        margin-right: 0;
+    }
+
+    .infos {
+        background-color: #555;
+        padding: 16px;
+    }
+    .infos .name {
+        font-size: 22px;
+    }
+    .infos .name, .infos .description, .infos .taken, .infos .coordinates, .infos .details {
+        color: white;
+        padding-bottom: 8px;
+        overflow: hidden;
     }
 </style>
