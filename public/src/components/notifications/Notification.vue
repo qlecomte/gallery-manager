8623de3af19e0f36f3a946fad9a822e9212c14f6
Quentin Lecomte <quentinh.lecomte@gmail.com>
2019-07-07 04:14:05 +0200

    Add Notification components

---
+++ public/src/components/notifications/Notification.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="message">
+        <div class="message-icon">
+            <slot name="icon"/>
+        </div>
+        <div class="message-body">
+            <slot/>
+        </div>
+        <!--<button class="message-close">-->
+            <!--<CloseIcon/>-->
+        <!--</button>-->
+    </div>
+</template>
+<style scoped lang="scss">
+    .message {
+        background-color: #1976d2;
+        width: 400px;
+        display: flex;
+
+        .message-icon {
+            background-color: rgba(0,0,0,0.25);
+            width: 80px;
+            display: flex;
+            align-items: center;
+
+            svg {
+                height: 32px;
+                fill: white;
+            }
+        }
+
+        .message-body {
+            color: white;
+            padding: 16px;
+        }
+    }
+</style>
+<script>
+    import CloseIcon from '../../../images/arrows/cancel.svg'
+
+    export default {
+      name: 'Notification',
+      components: {
+        CloseIcon
+      }
+    }
+</script>
---
+++ public/src/components/notifications/NotificationBubble.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bubble">
+        <BellIcon class="icon"/>
+    </div>
+</template>
+<style scoped lang="scss">
+    .bubble {
+        height: 60px;
+        width: 60px;
+        border-radius: 30px;
+        background-color: dodgerblue;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .icon {
+            fill: white;
+            width: 30px;
+            height: 30px;
+
+        }
+    }
+</style>
+<script>
+  import BellIcon from '../../../images/notification.svg'
+
+  export default {
+    name: 'NotificationBubble',
+    components: {
+      BellIcon
+    }
+  }
+</script>
